bf47c4e17becbed78a0aeb45add1ed8075ad0b09
kdjun97 <kdjun97@gmail.com>
2022-07-19 17:57:06 +0900

    Feat: Add counsel activity 전문가 상담 activity 제작중

---
+++ app/src/main/java/com/example/nuru/view/adapter/CounselAdapter.kt
@@ -0,0 +1,10 @@
+package com.example.nuru.view.adapter
+
+import android.content.Context
+import androidx.recyclerview.widget.ListAdapter
+import com.example.nuru.viewmodel.counsel.CounselViewModel
+
+class CounselAdapter (private val context: Context, private val viewModel : CounselViewModel) :
+    ListAdapter<Comments, CounselAdapter.CounselViewHolder>(CounselAdapter.DIFF_CALLBACK) {
+
+}
