791d0fc96853daa1ab3bbb09e46e8e6b7d2d9aa6
kdjun97 <kdjun97@gmail.com>
2022-07-20 12:56:38 +0900

    Feat: Add cousel activity 전문가 상담 activity 추가 - 전화 걸기 기능 구현 완료(DIAL) - Firebase DB 값 read, 전문가 CardView show - 현재 버전에서는 수동으로 DB에 전문가 정보를 올림

--- app/src/main/java/com/example/nuru/viewmodel/counsel/CounselViewModel.kt
+++ app/src/main/java/com/example/nuru/viewmodel/counsel/CounselViewModel.kt
@@ -1,6 +1,16 @@
 package com.example.nuru.viewmodel.counsel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.nuru.model.data.counsel.NameCard
+import com.example.nuru.repository.counsel.CounselRepository
 
 class CounselViewModel : ViewModel() {
+    private val repo = CounselRepository()
+    private val nameCardList = repo.getNameCardList
+
+    fun fetchData() : LiveData<MutableList<NameCard>> {
+        return nameCardList
+    }
 }
