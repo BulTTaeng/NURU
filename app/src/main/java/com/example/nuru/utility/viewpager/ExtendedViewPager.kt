04aaef9999a0be459c58fe4a9f57e7bde18f34ad
BulTTaeng <wogur7496@gmail.com>
2022-07-22 17:37:33 +0900

    Back up: save branch viewPager2에서 확대 이벤트를 위한 backup

---
+++ app/src/main/java/com/example/nuru/utility/viewpager/ExtendedViewPager.kt
@@ -0,0 +1,10 @@
+package com.example.nuru.utility.viewpager
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+
+
+
